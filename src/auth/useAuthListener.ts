f38254fbc1d0f1dcc53c5cbbd0de2404a2d7de4a
aryanc0des <134934556+aryanc0des@users.noreply.github.com>
2026-01-22 11:39:07 +0530

    minor fixes

--- src/auth/useAuthListener.ts
+++ src/auth/useAuthListener.ts
@@ -1,21 +1,22 @@
 //ONE GOAL:- HAS USER LOGGED IN OR NO???, TO SHOW THE RIGHT SCREEN
 
-import { onAuthStateChanged } from '@react-native-firebase/auth'
+import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth'
 import {useState, useEffect} from 'react'
 
 
 export function useAuth(){
-    //state here
+    
     const [user, setUser] = useState(null)
     const [initializing, setInitializing] = useState(true) // important in order to have a non buggy app and have a loading screen state while we know about auth state
-    //effect here
+    
     useEffect(()=>{
+        const auth = getAuth()
         const unsubscribe = onAuthStateChanged(auth, (user)=>{ //this listener returns a function which unsubscribes or stops the listener from listening 
             setUser(user)
             setInitializing(false)
         })
         return unsubscribe // returning this helps in unsubscribing or stopping the listener
     }, [])
-    //return here
+    
     return {user, initializing}
 }
