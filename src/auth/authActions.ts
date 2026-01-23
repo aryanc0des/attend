f3716c1723973c106a17eeed68e01d675e230496
aryanc0des <134934556+aryanc0des@users.noreply.github.com>
2026-01-23 23:59:22 +0530

    minor changes

---
+++ src/auth/authActions.ts
@@ -0,0 +1,15 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; 
+
+export function signIn(email: string, password: string){
+    const auth = getAuth()
+
+    try{
+        const userCred = signInWithEmailAndPassword(auth, email, password) // signs in user
+        console.log("user signed in", userCred)
+    }
+    catch(err){
+        console.log("Error:", err)
+    }
+}
+//
+
